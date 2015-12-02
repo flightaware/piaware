b8928dc1c1d8dedfdea70d807afbdc77c2ad5184
Oliver Jowett <oliver@mutability.co.uk>
2015-12-02 18:51:19 +0000

    Version bump.

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -9,8 +9,8 @@
 
 set serverRetryIntervalSeconds 60
 
-set piawareVersion 2.1
-set piawareVersionFull 2.1-3
+set piawareVersion 2.2
+set piawareVersionFull 2.2.0~dev
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
