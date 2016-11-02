b49fa83454659cb4995bdb034e13cbfcedba137a
Oliver Jowett <oliver@mutability.co.uk>
2016-11-02 23:26:32 +0000

    Dev version, redux.

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -9,8 +9,8 @@
 
 set serverRetryIntervalSeconds 60
 
-set piawareVersion 3.2
-set piawareVersionFull 3.2.0~dev
+set piawareVersion 3.3
+set piawareVersionFull 3.3.0~dev
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
