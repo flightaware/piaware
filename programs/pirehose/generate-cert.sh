55eac6a226626725ffbc932d0f403be89b97f896
Oliver Jowett <oliver@mutability.co.uk>
2018-07-09 23:40:44 +0100

    Move pirehose key generation into a separate systemd service & helper script

---
+++ programs/pirehose/generate-cert.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -e /etc/piaware/pirehose.cert.pem ] && [ ! -e /etc/piaware/pirehose.key.pem ]
+then
+    echo "Generating pirehose self-signed certificate.." >&2
+    mkdir -p /etc/piaware
+    openssl req -x509 -newkey rsa:4096 -keyout /etc/piaware/pirehose.key.pem -out /etc/piaware/pirehose.cert.pem -days 3650 -nodes -subj /CN=pirehose -batch
+    chown root:piaware /etc/piaware/pirehose.key.pem
+    chmod 0640 /etc/piaware/pirehose.key.pem
+    chmod 0644 /etc/piaware/pirehose.cert.pem
+fi
