4dd4c19243d70afdb2cbf52cc1284834857e3e26
Oliver Jowett <oliver@mutability.co.uk>
2019-11-07 13:16:26 +0800

    3.8.0~dev

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -9,8 +9,8 @@
 
 set serverRetryIntervalSeconds 60
 
-set piawareVersion 3.7
-set piawareVersionFull 3.7.2
+set piawareVersion 3.8
+set piawareVersionFull 3.8.0~dev
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
