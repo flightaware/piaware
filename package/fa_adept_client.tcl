4f8d9ae0e220a2c51e2015967f76d72d0ddc26e9
Oliver Jowett <oliver.jowett@flightaware.com>
2019-12-11 15:01:20 -0600

    Update adept host fallback IPs

--- package/fa_adept_client.tcl
+++ package/fa_adept_client.tcl
@@ -18,7 +18,7 @@ set caDir [file join [file dirname [info script]] "ca"]
 
 ::itcl::class AdeptClient {
 	public variable sock
-	public variable hosts [list piaware.flightaware.com piaware.flightaware.com [list 70.42.6.197 70.42.6.198 70.42.6.191 70.42.6.225 70.42.6.224 70.42.6.156]] shuffle_hosts
+	public variable hosts [list piaware.flightaware.com piaware.flightaware.com [list 70.42.6.228 70.42.6.224 70.42.6.198 70.42.6.156 70.42.6.225]] shuffle_hosts
 	public variable port 1200
 	public variable loginTimeoutSeconds 15
 	public variable connectRetryIntervalSeconds 60
