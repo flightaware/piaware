9995a0413564de87e1c6f8743df0c5b4eb95f5e8
Oliver Jowett <oliver.jowett@flightaware.com>
2019-12-16 12:54:46 -0600

    Stage 3.8.0 release

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -10,7 +10,7 @@
 set serverRetryIntervalSeconds 60
 
 set piawareVersion 3.8
-set piawareVersionFull 3.8.0~dev
+set piawareVersionFull 3.8.0
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
