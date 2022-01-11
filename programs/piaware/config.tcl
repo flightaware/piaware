fcf047a79b432897a1374430dc6fe2d6b47e33d7
Oliver Jowett <oliver.jowett@flightaware.com>
2022-01-11 13:15:45 +0800

    Bump to 7.1~dev

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -9,8 +9,8 @@
 
 set serverRetryIntervalSeconds 60
 
-set piawareVersion 7.0
-set piawareVersionFull 7.0
+set piawareVersion 7.1~dev
+set piawareVersionFull 7.1~dev
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
