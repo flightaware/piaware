7b5057f6d6b50ca64d33595a07aa43d9bcbb0d9e
eric1tran <eric1tran@gmail.com>
2022-11-21 16:52:19 +0000

    Version bump to 8.0

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -9,8 +9,8 @@
 
 set serverRetryIntervalSeconds 60
 
-set piawareVersion 8.0~dev
-set piawareVersionFull 8.0~dev
+set piawareVersion 8.0
+set piawareVersionFull 8.0
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
