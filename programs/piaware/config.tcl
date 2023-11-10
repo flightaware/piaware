18aaf464b49887de6afafc10cac672a74a3ac1f1
Oliver Jowett <oliver.jowett@flightaware.com>
2023-11-10 11:55:44 +0800

    Prepare for 9.0.1 bugfix release

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -10,7 +10,7 @@
 set serverRetryIntervalSeconds 60
 
 set piawareVersion 9.0
-set piawareVersionFull 9.0
+set piawareVersionFull 9.0.1
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
