6f006249c2b92dde8250ebe3eb976cb774ada70c
eric1tran <eric1tran@gmail.com>
2025-01-21 18:42:50 +0000

    Update comments

--- package/fa_piaware_config.tcl
+++ package/fa_piaware_config.tcl
@@ -1082,12 +1082,13 @@ namespace eval ::fa_piaware_config {
 	#
 	#  priority 100+:     any extra config file given as an argument (readonly)
 	#  priority 100+:     any config files found in /media/usb/*/piaware-config.txt, ordered arbitrarily (readonly)
+	#  priority 50:       /boot/piaware-config.txt (readwrite)
 	#  priority 50:       /boot/firmware/piaware-config.txt (readwrite)
 	#  priority 40:       /etc/piaware.conf (readwrite)
 	#  priority 30:       /usr/share/piaware-support/piaware-image-config.txt (readonly) (provides additional defaults on PiAware sdcard images)
 	#
 	# which means that in general changes will be written to /etc/piaware.conf where possible, or
-	# /boot/firmware/piaware-config.txt if the setting was set there.
+	# in piaware-config.txt if the setting was set there.
 	#
 	# Provide a itcl name pattern (e.g. #auto) as "name"
 	proc piaware_combined_config {name {extraConfigFile ""}} {
