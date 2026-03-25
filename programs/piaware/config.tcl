a835999a7dda9ce40830d7f251d1120dc44778f3
Eric Tran <eric.tran@flightaware.com>
2026-03-25 14:26:16 -0500

    bump version to 11.0~dev

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -9,8 +9,8 @@
 
 set serverRetryIntervalSeconds 60
 
-set piawareVersion 10.2
-set piawareVersionFull 10.2
+set piawareVersion 11.0~dev
+set piawareVersionFull 11.0~dev
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
