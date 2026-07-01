8480286b449e88d8446bc56068ef80eb2d8c8c82
zwang <30325083+deadlywang@users.noreply.github.com>
2026-07-01 13:26:09 -0500

    bump the official version to 11.1

--- programs/piaware/config.tcl
+++ programs/piaware/config.tcl
@@ -9,8 +9,8 @@
 
 set serverRetryIntervalSeconds 60
 
-set piawareVersion 11.1~alpha
-set piawareVersionFull 11.1~alpha
+set piawareVersion 11.1
+set piawareVersionFull 11.1
 
 # how many seconds with no messages received from the ADS-B receiver before
 # we will attempt to restart dump1090
